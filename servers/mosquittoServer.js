cd787d658f5220390097984931e01a45159d0ae9
Cefn Hoile <github@cefn.com>
2015-12-14 11:47:53 +0000

    Added check for startup completed by monitoring standard out

--- servers/mosquittoServer.js
+++ servers/mosquittoServer.js
@@ -5,8 +5,20 @@ function Daemon(){}
 
 Daemon.prototype = {
     launch:function(cb){
-        this.server = spawn("/usr/local/sbin/mosquitto", ["--config-file", "./mosquitto.conf", "--verbose"], {cwd:__dirname});
-        cb(); //TODO should something be awaited in Posixland?
+        var that = this;
+        that.server = spawn("/usr/local/sbin/mosquitto", ["--config-file", "./mosquitto.conf", "--verbose"], {cwd:__dirname});
+        function consoleHandler(consoleData){
+            var consoleString = consoleData.toString();
+            if(consoleString.indexOf("Opening websockets listen socket on port 3000") !== -1){
+                that.server.stdout.removeListener("data", consoleHandler);
+                that.server.stderr.removeListener("data", consoleHandler);
+                var _cb = cb;
+                cb = null;
+                _cb();
+            }
+        }
+        that.server.stdout.on("data", consoleHandler);
+        that.server.stderr.on("data", consoleHandler);
     },
     kill:function(cb){
         var that = this;
